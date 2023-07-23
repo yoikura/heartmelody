1a6d2af1d11940589c82839793dce59886009243
yoikura <92100741+yoikura@users.noreply.github.com>
2023-07-23 16:57:10 +0800

    Add files via upload

---
+++ heartmelody-app/App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch,1111')
+			uni.hideTabBar()
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	page {
+		background-color: rgba(235, 236, 237, 1);
+	}
+</style>
